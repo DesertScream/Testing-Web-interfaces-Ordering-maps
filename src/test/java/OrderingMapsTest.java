ec365299bc6177483015711db9c8b351104e577b
DesertScream <www.anton.dzuin@gmail.com>
2022-05-07 22:45:36 +0500

    refactor4

--- src/test/java/OrderingMapsTest.java
+++ src/test/java/OrderingMapsTest.java
@@ -1,20 +1,10 @@
-import com.codeborne.selenide.Condition;
+
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.impl.SelenideElementDescriber;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-
 public class OrderingMapsTest {
 
     @BeforeEach
