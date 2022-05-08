f117ce80c0313277485425d05e678513ea9f5a00
DesertScream <www.anton.dzuin@gmail.com>
2022-05-08 17:59:13 +0500

    add new tests

--- src/test/java/OrderingMapsTest.java
+++ src/test/java/OrderingMapsTest.java
@@ -2,7 +2,7 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 public class OrderingMapsTest {
@@ -16,14 +16,62 @@ public class OrderingMapsTest {
     @Test
     void shouldSuccessfully() {
 
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
 
         $x("//*[@name=\"name\"]").setValue("Антон");
-//        $("[name=\"name\"]").setValue("Антон");
         $x("//*[@name=\"phone\"]").setValue("+79060000000");
-//        $("[name=\"phone\"]").setValue("+79060000000");
         $x("//*[@data-test-id=\"agreement\"]").click();
         $x("//*[@type=\"button\"]").click();
         $(withText("Ваша заявка успешно отправлена!")).shouldHave(visible);
+    }
+
+    @Test
+    void shouldNotCorrectName1() {
+
+        Configuration.holdBrowserOpen = false;
+
+        $x("//*[@data-test-id=\"agreement\"]").click();
+        $x("//*[@name=\"name\"]").setValue("Anton Dz");
+        $x("//*[@name=\"phone\"]").setValue("+79060000000");
+        $x("//*[@type=\"button\"]").click();
+        $(withText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."))
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldBeVoidArea1() {
+
+        Configuration.holdBrowserOpen = false;
+
+        $x("//*[@data-test-id=\"agreement\"]").click();
+        $x("//*[@name=\"name\"]").setValue("");
+        $x("//*[@name=\"phone\"]").setValue("+79060000000");
+        $x("//*[@type=\"button\"]").click();
+        $(withText("Поле обязательно для заполнения")).shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotCorrectName2() {
+
+        Configuration.holdBrowserOpen = false;
+
+        $x("//*[@data-test-id=\"agreement\"]").click();
+        $x("//*[@name=\"name\"]").setValue("Антон");
+        $x("//*[@name=\"phone\"]").setValue("qwerty");
+        $x("//*[@type=\"button\"]").click();
+        $(withText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."))
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+    @Test
+    void shouldBeVoidArea2() {
+
+        Configuration.holdBrowserOpen = false;
+
+        $x("//*[@data-test-id=\"agreement\"]").click();
+        $x("//*[@name=\"name\"]").setValue("Антон");
+        $x("//*[@name=\"phone\"]").setValue("");
+        $x("//*[@type=\"button\"]").click();
+        $(withText("Поле обязательно для заполнения"))
+                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 }
